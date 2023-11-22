06aa812bab081a2807ac0d76dd74507c9a78c0ae
JohnQuill <fedorhodyakov@gmail.com>
2023-11-22 10:23:02 +0300

    Исправление доступа к бд

--- src/test/java/DataReadWriteTest.java
+++ src/test/java/DataReadWriteTest.java
@@ -23,7 +23,7 @@ public class DataReadWriteTest {
     @Before
     public void start() throws SQLException {
         connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/sportschool",
-                "admin", System.getenv("PASSW"));
+                "admin", "admin");
         Statement statement = connection.createStatement();
         sportsmanDao = new SportsmanDaoImpl(statement);
         sectionDao = new SectionDaoImpl(statement);
