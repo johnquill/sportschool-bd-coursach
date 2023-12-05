f6e4a118b05a15a56b261a69ec39a91d61b605e1
JohnQuill <fedorhodyakov@gmail.com>
2023-12-05 23:00:34 +0300

    Исправление файла импорта бд

--- src/main/resources/sportschool.sql
+++ src/main/resources/sportschool.sql
@@ -15,8 +15,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE IF NOT EXISTS sportschool;
-USE spotschool;
+USE sportschool;
 --
 -- Table structure for table `coach`
 --
