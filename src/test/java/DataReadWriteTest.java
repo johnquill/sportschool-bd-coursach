29353159869a870297793939e6f4392fef9eefbf
JohnQuill <fedorhodyakov@gmail.com>
2023-12-05 23:12:31 +0300

    Вычисление метода с ошибкой

--- src/test/java/DataReadWriteTest.java
+++ src/test/java/DataReadWriteTest.java
@@ -34,13 +34,13 @@ public class DataReadWriteTest {
             Coach coach = new Coach(333L, "Шкуратов", "Андрей", "Владимирович", "Баскетбол");
             coachDao.add(coach);
 
-            String[] trainer = new String[] {"Шкуратов", "Андрей", "Владимирович"};
+            /*String[] trainer = new String[] {"Шкуратов", "Андрей", "Владимирович"};
             Section section = new Section(444L, "Баскетбол", "Сб 20:00", 221,
                     "С собой водичку", true, "Баскетбол", trainer);
             sectionDao.add(section);
 
             Sportsman sportsman = new Sportsman(500L, "Ходяков", "Федор", "Андреевич", "Баскетбол", "Программист");
-            sportsmanDao.add(sportsman);
+            sportsmanDao.add(sportsman);*/
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
