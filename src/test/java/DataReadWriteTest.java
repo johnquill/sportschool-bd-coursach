5b5b5cf598dde8ba452664cc254897190a82f5a9
JohnQuill <fedorhodyakov@gmail.com>
2023-12-06 23:05:57 +0300

    добавление тестов на удаление и изменение данных в базе
    (cherry picked from commit cf6eb0e9c0cdf0a2d86361d300eacf29ed72cf90)

--- src/test/java/DataReadWriteTest.java
+++ src/test/java/DataReadWriteTest.java
@@ -1,49 +1,12 @@
-import model.dao.CoachDaoImpl;
-import model.dao.SectionDaoImpl;
-import model.dao.SportsmanDaoImpl;
-import model.entity.Coach;
-import model.entity.Section;
-import model.entity.Sportsman;
 import org.junit.*;
-import org.junit.runners.MethodSorters;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-@FixMethodOrder(MethodSorters.JVM)
-public class DataReadWriteTest {
-
-    SportsmanDaoImpl sportsmanDao;
-    SectionDaoImpl sectionDao;
-    CoachDaoImpl coachDao;
-    Connection connection;
+public class DataReadWriteTest extends DataBaseTest {
 
     @Before
     public void start() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/sportschool",
-                "root", System.getenv("PASSW"));
-        Statement statement = connection.createStatement();
-        sportsmanDao = new SportsmanDaoImpl(statement);
-        sectionDao = new SectionDaoImpl(statement);
-        coachDao = new CoachDaoImpl(statement);
-        connection.setAutoCommit(false);
-
-        try {
-            Coach coach = new Coach(333L, "Шкуратов", "Андрей", "Владимирович", "Баскетбол");
-            coachDao.add(coach);
-
-            String[] trainer = new String[] {"Шкуратов", "Андрей", "Владимирович"};
-            Section section = new Section(444L, "Баскетбол", "Сб 20:00", 221,
-                    "С собой водичку", true, "Баскетбол", trainer);
-            sectionDao.add(section);
-
-            Sportsman sportsman = new Sportsman(500L, "Ходяков", "Федор", "Андреевич", "Баскетбол", "Программист");
-            sportsmanDao.add(sportsman);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        init();
     }
 
     @After
