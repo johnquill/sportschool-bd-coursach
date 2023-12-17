b0afd9e5633ed4d183f29b9866084dcec70e8a79
JohnQuill <fedorhodyakov@gmail.com>
2023-12-18 00:11:12 +0300

    Revert "исправление файла импорта базы данных"
    This reverts commit 858a1d69b6f2cf19954338621515104a2f5683a3.

--- src/main/resources/sportschool.sql
+++ src/main/resources/sportschool.sql
@@ -15,7 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-GRANT ALL PRIVILEGES ON sportschool.* TO 'admin'@'localhost';
 USE sportschool;
 --
 -- Table structure for table `coach`
